bf5b193e869fbc69acc16d93caad900f9561dfad
snezhanakolesnik <sizma01ecommerce@gmail.com>
2026-01-28 18:40:07 +0200

    Initial commit

--- main.py
+++ main.py
@@ -7,26 +7,28 @@ def main():
 
     while True:
         print(f'\n ----ToDo APP----')
-        print(f'1. Показать список')
-        print(f'2. Добавить задачу')
-        print(f'3. Отметить задачу выполненной')
-        print(f'4. Сохранить и выйти')
+
+        if not todo.tasks:
+            print('\n Task List is empty')
+        else:
+            for i, task in enumerate(todo.tasks, 1):
+                print(f'{i}. {task}')
+
+        print(f'\n1. Добавить задачу')
+        print(f'2. Отметить задачу выполненной')
+        print(f'3. Сохранить и выйти')
 
         choice = input("Выберите действие: ")
 
         if choice == '1':
-            for i, task in enumerate(todo.tasks):
-                print(i + 1, task)
-
-        elif choice == '2':
             text = input("Введите задачу: ")
             todo.add_task(Task(text))
 
-        elif choice == '3':
+        elif choice == '2':
             num = int(input("Номер задачи: "))
             todo.tasks[num - 1].completed = True
 
-        elif choice == '4':
+        elif choice == '3':
             todo.save_file()
             print("До скорой встречи!")
             break
